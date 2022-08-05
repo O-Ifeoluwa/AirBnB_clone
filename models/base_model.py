2a37c005bf3ce30cb790b5007301b74d16544603
O-Ifeoluwa <osatuyiIfeoluwa@gmail.com>
2022-08-05 18:29:56 +0100

    store first object

--- models/base_model.py
+++ models/base_model.py
@@ -2,14 +2,24 @@
 """Base module"""
 import uuid
 from datetime import datetime
+from models import storage
 
 class BaseModel():
     """BaseModel defines all common attributes/methods for other classes"""
-    def __init__(self, id, created_at, updated_at):
+    def __init__(self, *args, **kwargs):
         """initializes the attributes of BaseModel"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                elif key == '__class__':
+                    continue
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """string representation of the object"""
@@ -18,6 +28,7 @@ class BaseModel():
     def save(self):
         """updates the public instance `updated_at` with the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__ of the instance"""
---
+++ models/engine/file_storage.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""serialization and deserialization"""
+import json
+
+class FileStorage():
+    """serializes instances to JSON file and deserializes JSON files to instances"""
+    __file_path = 'file.json'
+    __objects = {}
+
+    def __init__(self):
+        """initializing method"""
+        super().__init__()
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        if obj:
+            key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+            self.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file"""
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
+            json.dumps(FileStorage.__objects, f, default=str)
+
+    def reload(self):
+        """deserializes the JSON file to __objects - only if the JSON file exists"""
+       try:
+           with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
+            FileStorage.__objects = json.load(f)
+        except Exception:
+            pass
+
