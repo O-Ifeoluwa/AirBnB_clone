65807c1b84944430ffa0e10ec393aefc57144699
O-Ifeoluwa <osatuyiIfeoluwa@gmail.com>
2022-08-05 18:38:51 +0100

    pycodestyle check

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import uuid
 from datetime import datetime
 from models import storage
 
+
 class BaseModel():
     """BaseModel defines all common attributes/methods for other classes"""
     def __init__(self, *args, **kwargs):
@@ -23,15 +24,18 @@ class BaseModel():
 
     def __str__(self):
         """string representation of the object"""
-        print("{[]} {[]} {}".format(self.__class__.__name__, self.id, self.__dict__))
+        print("{[]} {[]} {}".format(self.__class__.__name__, 
+            self.id, self.__dict__))
 
     def save(self):
-        """updates the public instance `updated_at` with the current datetime"""
+        """updates the public instance `updated_at` 
+            with the current datetime"""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        """returns a dictionary containing all keys/values 
+            of __dict__ of the instance"""
         my_dict = self.__dict__.copy()
         my_dict['__class__'] = self.__class__.__name__
         my_dict['created_at'] = self.created_at.isoformat()
