44b5e145973100f3cc84f32b5cf30f26e4b5d72a
O-Ifeoluwa <osatuyiIfeoluwa@gmail.com>
2022-08-05 18:54:06 +0100

    pycodestyle check

--- models/base_model.py
+++ models/base_model.py
@@ -25,7 +25,7 @@ class BaseModel():
     def __str__(self):
         """string representation of the object"""
         return "{[]} {[]} {}".format(self.__class__.__name__,
-                self.id, self.__dict__)
+                 self.id, self.__dict__)
 
     def save(self):
         """
