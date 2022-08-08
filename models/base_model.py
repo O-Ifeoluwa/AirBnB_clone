8967c9921725a4dcc57837d3411f5b1addfcea0c
O-Ifeoluwa <osatuyiIfeoluwa@gmail.com>
2022-08-08 13:25:30 +0100

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -16,6 +16,9 @@ class BaseModel():
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 elif key == '__class__':
                     continue
+
+
+                setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
